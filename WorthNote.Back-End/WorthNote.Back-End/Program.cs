5c3554905d860a8c385a0fe7b8312da359827364
Farid <faridbusiness@bk.ru>
2023-07-17 18:54:31 +0200

    Added back-end project template

---
+++ WorthNote.Back-End/WorthNote.Back-End/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorthNote.Back_End
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
