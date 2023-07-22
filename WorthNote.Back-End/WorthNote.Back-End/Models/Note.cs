e80c1927c1d6bfccf2935b5f7f239f1651fec398
ab-gul <ab.gulchohra@gmail.com>
2023-07-22 22:57:45 +0200

    added note model

---
+++ WorthNote.Back-End/WorthNote.Back-End/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorthNote.Back_End.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "Unnamed";
+        public string Description { get; set; } = string.Empty; 
+        public DateTime AddedTime { get; }  = DateTime.Now;
+
+
+        
+    }
+}
